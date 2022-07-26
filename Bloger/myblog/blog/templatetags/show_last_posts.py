2b744cf137d3736902044b6bf7b567317b1fa863
Yankeegohome <focusnick28@gmail.com>
2022-07-27 02:18:44 +0300

    added banner

--- Bloger/myblog/blog/templatetags/show_last_posts.py
+++ Bloger/myblog/blog/templatetags/show_last_posts.py
@@ -9,3 +9,8 @@ def show_posts(menu_class='content'):
     posts = Post.objects.all().order_by("-created_at")[0:5]
     return {"posts": posts, "menu_class": menu_class}
 
+
+@register.inclusion_tag('blog/banner.html')
+def show_banner(menu_class='owl-banner owl-carousel'):
+    posts = Post.objects.all().order_by("-created_at")[0:5]
+    return {"posts": posts, "menu_class": menu_class}
